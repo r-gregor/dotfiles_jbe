25fa7af596559d41edca6dba1f117125ba567b33
jbez0rg <yb3_z0rg@proton.me>
2024-11-05 07:24:11 +0100

    update 20241105_0724_jbe

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,12 +2,13 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
+# polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
 # Launch bar
 echo "---" | tee -a /tmp/polybar.log
 polybar bar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Bar launched..."
+
